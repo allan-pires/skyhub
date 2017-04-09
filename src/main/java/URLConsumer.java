bd05f9f43ddef936b2f1aea4b935e54a5e6cf1e1
Allan Victor <doislan@gmail.com>
2017-04-09 18:39:17 -0300

    Added URL Consumer

---
+++ src/main/java/URLConsumer.java
@@ -0,0 +1,42 @@
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class URLConsumer {
+
+    public JSONObject consume(String uri) {
+        try{
+            String lines = readFromConnection(getConnection(uri));
+            return new JSONObject(lines);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return new JSONObject();
+    }
+
+    private String readFromConnection(HttpURLConnection connection) throws IOException {
+        InputStream input = new BufferedInputStream(connection.getInputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+        String line;
+        StringBuilder builder = new StringBuilder();
+        while ((line = reader.readLine()) != null){
+            builder.append(line);
+        }
+
+        connection.disconnect();
+
+        return builder.toString();
+    }
+
+    private HttpURLConnection getConnection(String uri) throws IOException {
+        URL url = new URL(uri);
+
+        return (HttpURLConnection) url.openConnection();
+    }
+
+}
